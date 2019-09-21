98e7c82b902a05e09ec57d6a60727482fae020ef
Thanh An <an.laivanthanh29@gmail.com>
2019-09-21 11:19:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>UNESCO</title>
+	<title></title>
 </head>
 <body>
 	<h2 title="(United Nations Educational, Scientific and Cultural Organization)"
@@ -11,10 +11,14 @@
 		UNESCO
 		<i>(United Nations Educational, Scientific and Cultural Organization)</i>
 	</b>
+	<a href="BT259jgs.html"> BT 259</a>
 	<br>
 	<span style="font-size: 32px">Locations</span>
 	<span style="font-size: 16px">Headquarter of UNESCO is located in Paris, France.</span>
 	<a href="BT243.html">BT243</a>
+	<a href="BT304js.html"> BT 304</a>
+	<br>
+	<a href="BT305js.html"> BT 305</a>
 
 </body>
 </html>
