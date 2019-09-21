8cae0cb9085b68d9f77feab62c5efd68b848218c
Thanh An <an.laivanthanh29@gmail.com>
2019-09-21 11:24:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,67 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>BT259JS</title>
 </head>
 <body>
-	<h2 title="(United Nations Educational, Scientific and Cultural Organization)"
-> UNESCO </h2>
-	<hr>
-	<b>
-		UNESCO
-		<i>(United Nations Educational, Scientific and Cultural Organization)</i>
-	</b>
-	<a href="BT259jgs.html"> BT 259</a>
-	<br>
-	<span style="font-size: 32px">Locations</span>
-	<span style="font-size: 16px">Headquarter of UNESCO is located in Paris, France.</span>
-	<a href="BT243.html">BT243</a>
-	<a href="BT304js.html"> BT 304</a>
-	<br>
-	<a href="BT305js.html"> BT 305</a>
+	<table>
+		<tr>
+			<td>Nhập giá trị a</td>
+			<td>
+				<input type="text" name="A" id="hihi">
+			</td>
+		</tr>
+		<tr>
+			<td>Nhập giá trị b</td>
+			<td>
+				<input type="text" name="B" id="haha">
+			</td>
+		</tr>
+		<tr>
+			<td></td>
+			<td>
+				<input type="submit" name="kq" id="kq">
+			</td>
+		</tr>
+		<tr>
+			<td>Kết quả</td>
+			<td>
+				<input type="text" name="ketqua" id="ketqua">
+			</td>
+		</tr>
 
+		<script type="text/javascript">
+			function Kq_bacnhat(a, b){
+				if(a===0){
+					return null;
+				}
+				return -b/a;
+
+			}
+
+			document.getElementById("kq").onclick=function(){
+				let a = document.getElementById("hihi").value;
+				let b = document.getElementById("haha").value;
+
+				let c = Kq_bacnhat(parseFloat(a), parseFloat(b));
+				 
+				document.getElementById("ketqua").value = c;
+
+
+			}
+		</script>
+	</table>
+	<!-- <p> Giải phương trình bậc nhất (ax + b) = 0</p>
+	<p>Bước 1: Hiển thị prompt để nhập giá trị cho a</p>
+	<p>Bước 2: Nhập giá trị cho b bằng prompt</p>
+	<p>Bước 3: In ra kết quả</p>
+	
+	<script type="text/javascript">
+		a = prompt("Nhap gia tri a")
+		b = prompt("Nhap gia tri b")
+		x = -b/a 
+		alert(x)
+	</script>
+ -->
 </body>
 </html>
