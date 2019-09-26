992bf250e56bfbc87e8ec6db7a2944231ad44314
laivanthanhan <an.laivanthanh29@gmail.com>
2019-09-26 10:14:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
   <title></title>
 </head>
 <body>
-  <a href="file:///D:/New%20folder/html/BT259jgs.html"> BT 259</a>
+  <a href="html/BT259jgs.html"> BT 259</a>
   <br>
-  <a href="file:///D:/New%20folder/html/BT304js.html"> BT 304</a>
+  <a href="html/BT304js.html"> BT 304</a>
   <br>
-  <a href="file:///D:/New%20folder/html/BT305js.html"> BT 305</a>
+  <a href="html/BT305js.html"> BT 305</a>
 
 </body>
 </html>
