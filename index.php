728796ec158431caf8e5f8cbdc2ba0b5fdec0108
laivanthanhan <an.laivanthanh29@gmail.com>
2019-09-26 23:29:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,54 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<style type="text/css">
+		.ds_baitap{
+			width: 100%;
+			border: 1px solid #32a8a6;
+			background-color: #ee9600;
+		}
+
+		table{
+			padding-left: 30px;
+		}
+
+		tr a{
+			text-align: center;
+			font-family: justify;
+			font-size: 20px;
+			margin-left: -30px;
+		}
+
+		td {
+			display: inline-block;
+			width: 80px;
+			height: 30px;
+			padding-top: 10px;
+			padding-left: 30px;
+			color: red;
+
+		}
+		td :hover{
+			background-color: red;
+	
+		}
+	</style>
   <title></title>
 </head>
 <body>
-  <a href="html/BT244.html"> BT 244</a>
-  <br>
-  <a href="html/BT259jgs.html"> BT 259</a>
-  <br>
-  <a href="html/BT304js.html"> BT 304</a>
-  <br>
-  <a href="html/BT305js.html"> BT 305</a>
-
+	<div>
+		<h2>Bai tap:</h2>
+		<div  class="ds_baitap">
+			<table>
+				<tr>
+					<td><a href="html/BT244.html"> BT 244</a></td>
+					<td><a href="html/BT259jgs.html"> BT 259</a></td>
+					<td><a href="html/BT304js.html"> BT 304</a></td>
+					<td><a href="html/BT305js.html"> BT 305</a></td>
+				</tr>
+			</table>
+		</div>		
+	</div>
+	
 </body>
 </html>
